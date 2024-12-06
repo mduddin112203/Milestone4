1216df37026290846682511e5876faec54c21334
Ismail-Shaikh03 <123834113+Ismail-Shaikh03@users.noreply.github.com>
2024-12-06 13:28:13 -0500

    Update app.py with hosting stuff

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ from dash import dcc, html, Input, Output, State
 import pandas as pd
 import base64
 import io
+import os
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LinearRegression
 from sklearn.pipeline import Pipeline
@@ -228,5 +229,8 @@ def predict_target(n_clicks, input_values):
             return f"Error in prediction: {str(e)}"
     return "No prediction made yet."
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+#if __name__ == '__main__':
+ #   app.run_server(debug=True)
+if __name__ == "__main__":
+    port = int(os.environ.get('PORT', 8050))  
+    app.run_server(debug=True, port=port, host='0.0.0.0')
